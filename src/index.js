52745bcf3d54fb53dfb1487dc83aab5a027c4077
DariasKehl <neocount@gmail.com>
2021-02-10 11:27:12 -0500

    commit tree ae7410b7f3508173d25be72edd9b82c752c377f3

--- src/index.js
+++ src/index.js
@@ -13,3 +13,66 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// -- Display toys in collection
+let toyCol = document.getElementById('toy-collection');
+let configObj = {
+  headers: {
+    Accept: 'application/json',
+    Method: 'get'
+  }
+}
+
+function appendToDom(object) {
+  /* With the response data, make a <div class="card"> 
+  for each toy and add it to the toy-collection div.
+  */
+  
+  object.forEach(function (el) {
+    let appendDiv = document.createElement('div')
+    appendDiv.class = "card";
+    
+
+  })
+}
+
+fetch("http://localhost:3000/toys", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    appendToDom(object);
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert(`Error: ${error.message}`);
+    console.log(error.msg);
+})
+
+/* 
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+
+// method: "POST" is missing from the object below
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); 
+*/
